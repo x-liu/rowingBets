4bc8a25819d52bf3a4952b28a96a60b46a90d33f
Xing Liu <xingliu321@gmail.com>
2018-10-20 21:11:13 -0400

    second commit

---
+++ src/main/java/com/rowing/rowingBets/RowingBetsApplication.java
@@ -0,0 +1,12 @@
+package com.rowing.rowingBets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowingBetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RowingBetsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rowing/rowingBets/RowingBetsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rowing.rowingBets;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RowingBetsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
